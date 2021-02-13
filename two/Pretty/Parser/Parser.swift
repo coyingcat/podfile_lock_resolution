805f40c6bd6cfc04f31dea57233e6dbf962fdb3a
邓江洲 <dengjiangzhouReal@gmail.com>
2021-02-14 03:44:13 +0800

    do regex

--- two/Pretty/Parser/Parser.swift
+++ two/Pretty/Parser/Parser.swift
@@ -29,6 +29,10 @@ struct Parser {
         let list = info.split(separator: tag.lineEnd)
         var result = [String: [String]]()
         for item in list{
+            print(item)
+            let check = String(item).regex(pattern: "  \"?(.+) \\(?.+\\)")
+            // print(check)
+            
             
         }
         return nil
@@ -52,4 +56,24 @@ extension String{
     func rm(header str: String) -> String{
         return String(dropFirst(str.count))
     }
+
+
+  func regex(pattern: String) -> [String]?{
+    do {
+        let regex = try NSRegularExpression(pattern: pattern, options: NSRegularExpression.Options(rawValue: 0))
+        let all = NSRange(location: 0, length: count)
+        var matches = [String]()
+        regex.enumerateMatches(in: self, options: NSRegularExpression.MatchingOptions(rawValue: 0), range: all) {
+            (result : NSTextCheckingResult?, _, _) in
+              if let r = result {
+                    let nsstr = self as NSString
+                    let result = nsstr.substring(with: r.range) as String
+                    matches.append(result)
+              }
+        }
+        return matches
+    } catch {
+        return nil
+    }
+  }
 }
