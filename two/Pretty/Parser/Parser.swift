39ec8cb39ff2717a9d08f9d1ed1b740e5a92e709
邓江洲 <dengjiangzhouReal@gmail.com>
2021-02-14 03:10:21 +0800

    开发  +

--- two/Pretty/Parser/Parser.swift
+++ two/Pretty/Parser/Parser.swift
@@ -13,13 +13,11 @@ struct Parser {
     
     func one(_ content: String) -> [String: [String]]?{
         struct Tag{
-            let pageStart: String
+            let pageStart = "PODS:\n"
             let lineStart = "  "
             let sublineStart: String
-            let lineEnd = "\n"
-            let split = "DEPENDENCIES:"
+            let lineEnd: Character = "\n"
             init() {
-                pageStart = "PODS:" + lineEnd
                 sublineStart = lineStart + lineStart
             }
         }
@@ -27,50 +25,13 @@ struct Parser {
         guard content.hasPrefix(tag.pageStart) else {
             return nil
         }
-        var info = content.rm(header: tag.pageStart)
-        
-        var i = 0
-        let total = info.count
-
+        let info = content.rm(header: tag.pageStart)
+        let list = info.split(separator: tag.lineEnd)
         var result = [String: [String]]()
-
-        while i < total {
+        for item in list{
             
-            let endIndex = i + target.start.count - 1
-            if endIndex >= total{
-                break
-            }
-            
-            if let temp = info[i...endIndex], temp == target.start{
-
-                var bracketCount = 0
-                
-                i += target.start.count - 1
-                var beginIndex = 0
-                while i < total {
-                    switch info[i] {
-                    case target.second:
-                        if bracketCount == 0{
-                            beginIndex = i
-                        }
-                        bracketCount += 1
-                    case target.end:
-                        bracketCount -= 1
-                        if bracketCount == 0{
-                            result.append(info[(beginIndex+1)...(i-1)] ?? "")
-                        }
-                    default:
-                        ()
-                    }
-                    i += 1
-                }
-                
-            }
-            i += 1
         }
-        return result
-
-        
+        return nil
     }
     
     
