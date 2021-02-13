37a353366da3993ff761675b545a9bab5adbf54c
邓江洲 <dengjiangzhouReal@gmail.com>
2021-02-14 03:52:42 +0800

    do regex +

--- two/Pretty/Parser/Parser.swift
+++ two/Pretty/Parser/Parser.swift
@@ -12,30 +12,39 @@ import Foundation
 struct Parser {
     
     func one(_ content: String) -> [String: [String]]?{
+        
         struct Tag{
             let pageStart = "PODS:\n"
             let lineStart = "  "
             let sublineStart: String
             let lineEnd: Character = "\n"
+            let pageEnd: String
             init() {
                 sublineStart = lineStart + lineStart
+                pageEnd = "\(lineEnd)\(lineEnd)"
             }
         }
+        
         let tag = Tag()
         guard content.hasPrefix(tag.pageStart) else {
             return nil
         }
         let info = content.rm(header: tag.pageStart)
-        let list = info.split(separator: tag.lineEnd)
         var result = [String: [String]]()
-        for item in list{
-            print(item)
-            let check = String(item).regex(pattern: "  \"?(.+) \\(?.+\\)")
-            // print(check)
-            
-            
+        if let temp = info.components(separatedBy: tag.pageEnd).first{
+            let list = temp.split(separator: tag.lineEnd)
+            for item in list{
+                if let check = String(item).regex(pattern: "  \"?(.+) \\(?.+\\)"){
+                    print(check)
+                }
+            }
         }
-        return nil
+        if result.isEmpty{
+            return nil
+        }
+        else{
+            return result
+        }
     }
     
     
