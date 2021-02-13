8c0238551f9f98c00d5933b3a81a43ec3b7280ea
邓江洲 <dengjiangzhouReal@gmail.com>
2021-02-14 03:01:26 +0800

    开发

--- two/Pretty/Parser/Parser.swift
+++ two/Pretty/Parser/Parser.swift
@@ -10,7 +10,85 @@ import Foundation
 
 
 struct Parser {
-    static func parse(_ content: String) -> [String: [String]]?{
+    
+    func one(_ content: String) -> [String: [String]]?{
+        struct Tag{
+            let pageStart: String
+            let lineStart = "  "
+            let sublineStart: String
+            let lineEnd = "\n"
+            let split = "DEPENDENCIES:"
+            init() {
+                pageStart = "PODS:" + lineEnd
+                sublineStart = lineStart + lineStart
+            }
+        }
+        let tag = Tag()
+        guard content.hasPrefix(tag.pageStart) else {
+            return nil
+        }
+        var info = content.rm(header: tag.pageStart)
+        
+        var i = 0
+        let total = info.count
+
+        var result = [String: [String]]()
+
+        while i < total {
+            
+            let endIndex = i + target.start.count - 1
+            if endIndex >= total{
+                break
+            }
+            
+            if let temp = info[i...endIndex], temp == target.start{
+
+                var bracketCount = 0
+                
+                i += target.start.count - 1
+                var beginIndex = 0
+                while i < total {
+                    switch info[i] {
+                    case target.second:
+                        if bracketCount == 0{
+                            beginIndex = i
+                        }
+                        bracketCount += 1
+                    case target.end:
+                        bracketCount -= 1
+                        if bracketCount == 0{
+                            result.append(info[(beginIndex+1)...(i-1)] ?? "")
+                        }
+                    default:
+                        ()
+                    }
+                    i += 1
+                }
+                
+            }
+            i += 1
+        }
+        return result
+
+        
+    }
+    
+    
+    
+    func parse(_ content: String) -> [String: [String]]?{
+        
+        
+        one(content)
+        
+        
         return nil
+    }
+}
+
+
+
+extension String{
+    func rm(header str: String) -> String{
+        return String(dropFirst(str.count))
     }
 }
